541d938214a4c90cf22bb2171d02760c058b36be
Abhishek Chaurasia <abhishekch1999@gmail.com>
2025-12-28 11:41:52 +0530

    Handled memory leak

--- VendingMachineDesign/vendingMachineDesign.cpp
+++ VendingMachineDesign/vendingMachineDesign.cpp
@@ -1,222 +1,375 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<unordered_map>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
 using namespace std;
-enum class ItemType{
+enum class ItemType
+{
     Snack,
     Drink,
     Cookie
 };
 
+class Item
+{
+private:
+    std::string itemName;
+    int itemId;
+    ItemType itemType;
+    double price;
 
-class Item {
-    private:
-        std:: string itemName;
-        int itemId;
-        ItemType itemType;
-        double price;
-    public:
-        Item(std::string name, ItemType itemType, double price){
-            this->itemName = name;
-            this->itemType = itemType;
-            this->price = price;
-        }
-        std::string getItemName(){
-            return itemName;
-        }
-        ItemType getItemType(){
-            return itemType;
-        }
-        double getPrice(){
-            return price;
-        }
-        int getItemId(){
-            return itemId;
-        }
+public:
+    Item(int itemId, std::string name, ItemType itemType, double price)
+    {
+        this->itemId = itemId;
+        this->itemName = name;
+        this->itemType = itemType;
+        this->price = price;
+    }
+    std::string getItemName() const
+    {
+        return itemName;
+    }
+    ItemType getItemType() const
+    {
+        return itemType;
+    }
+    double getPrice() const
+    {
+        return price;
+    }
+    int getItemId() const
+    {
+        return itemId;
+    }
 };
 
-class Shelve{
-    private:
-        int shelveId;
-        ItemType itemType;
-        bool isOccupied;
-    public:
-        Shelve(int shelveId, ItemType itemType){
-            this->shelveId = shelveId;
-            this->itemType  = itemType;
-            this->isOccupied = false;
-        }
-        bool isShelveAvailable(ItemType shelveType){
-            if(isOccupied && this->itemType == shelveType) return true;
-            return false;
-        }
+class Shelve
+{
+private:
+    int shelveId;
+    ItemType itemType;
+    bool isOccupied;
 
-        bool vacateShelve(){
-            if(isOccupied==true){
-                isOccupied = false;
-                return true;
-            }
-            return false;
+public:
+    Shelve(int shelveId, ItemType itemType)
+    {
+        this->shelveId = shelveId;
+        this->itemType = itemType;
+        this->isOccupied = false;
+    }
+    bool isShelveAvailable(ItemType shelveType)
+    {
+        if (!isOccupied && this->itemType == shelveType)
+        {
+            return true;
         }
+        return false;
+    }
 
-        int getShelveId(){
-            return shelveId;
+    bool vacateShelve()
+    {
+        if (isOccupied == true)
+        {
+            isOccupied = false;
+            return true;
         }
-        ItemType getShelveType(){
-            return itemType;
-        }
+        return false;
+    }
 
-        bool assignShelve(){
-            if(isShelveAvailable(this->getShelveType())){
-                return true;
-            }
-            return false;
+    int getShelveId()
+    {
+        return shelveId;
+    }
+    ItemType getShelveType()
+    {
+        return itemType;
+    }
+
+    bool assignShelve()
+    {
+        if (isShelveAvailable(this->getShelveType()))
+        {
+            isOccupied = true;
+            return true;
         }
+        return false;
+    }
 };
 
-class Inventory{
-    private:
-        vector<Shelve*> shelves;
-        unordered_map<Item*,Shelve*> itemsPlaced;
-    public:
-        Inventory(){}
-        bool addItem(Item *item){
-            bool itemPlaced = false;
-            for(auto it=shelves.begin();it!=shelves.end();it++){
-                if((*it)->isShelveAvailable((*it)->getShelveType())){
-                    if((*it)->assignShelve()){
-                        itemsPlaced[item] = *it;
-                        itemPlaced = true;
-                        return itemPlaced;
-                    }
-                }
-            }
-            return itemPlaced;
+class Inventory
+{
+private:
+    unordered_map<Item *, Shelve *> itemsPlaced;
 
-        }
-        bool removeItem(Item *item){
-            bool itemRemoved=false;
-            Shelve* shelve = itemsPlaced[item];
-            if(shelve->vacateShelve()){
-                itemsPlaced.erase(item);
-                return true;
+public:
+    Inventory() {}
+    Item *isItemAvailable(int itemId)
+    {
+        for (auto it = itemsPlaced.begin(); it != itemsPlaced.end(); it++)
+        {
+            if ((*it).first->getItemId() == itemId)
+            {
+                return (*it).first;
             }
-            return false;
         }
+        return nullptr;
+    }
 
-        Item* isItemAvailable(int itemId){
-            for(auto it=itemsPlaced.begin();it!=itemsPlaced.end();it++){
-                if((*it).first->getItemId()==itemId){
-                    return (*it).first;
+    void placeItem(Item *item, Shelve *shelve)
+    {
+        itemsPlaced[item] = shelve;
+    }
+    void removeItem(Item *item)
+    {
+        itemsPlaced.erase(item);
+    }
+    Shelve *getItemShelve(Item *item)
+    {
+        return itemsPlaced[item];
+    }
+
+    vector<Item*> getListOfItems(){
+        vector<Item*> itemList;
+        for(auto it=itemsPlaced.begin();it!=itemsPlaced.end();it++){
+            itemList.push_back((*it).first);
+        }
+        return itemList;
+    }
+};
+
+class VendingMachine
+{
+private:
+    vector<Shelve *> shelves;
+    Inventory *inventory;
+
+public:
+    VendingMachine()
+    {
+        inventory = new Inventory();
+    }
+    bool addItem(Item *item)
+    {
+        bool itemPlaced = false;
+        for (auto it = shelves.begin(); it != shelves.end(); it++)
+        {
+            if(item->getItemType()==(*it)->getShelveType()){
+
+            
+            if ((*it)->isShelveAvailable((*it)->getShelveType()))
+            {
+                if ((*it)->assignShelve())
+                {
+                    inventory->placeItem(item, *it);
+                    itemPlaced = true;
+                    return itemPlaced;
                 }
-            }
+            }}
+        }
+        return itemPlaced;
+    }
+    bool removeItem(Item *item)
+    {
+        bool itemRemoved = false;
+        Shelve *shelve = inventory->getItemShelve(item);
+        if (shelve->vacateShelve())
+        {
+            inventory->removeItem(item);
+            return true;
+        }
+        return false;
+    }
+    Item *dispenseItem(int itemId)
+    {
+        Item *item = inventory->isItemAvailable(itemId);
+        if (item == nullptr)
+        {
             return nullptr;
         }
+        else
+        {
+            removeItem(item);
+            return item;
+        }
+    }
+
+    bool addShelve(Shelve *shelve)
+    {
+        shelves.push_back(shelve);
+        return true;
+    }
+    // bool removeShelve(Shelve *shelve)
+    // {
+    //     auto it = find(shelves.begin(), shelves.end(), shelve);
+    //     if (it != shelves.end())
+    //     {
+    //         shelves.erase(it);
+    //         return true;
+    //     }
+    //     return false; // Shelve not found
+    // }
+    ~VendingMachine()
+    {
+        vector<Item*> items = inventory->getListOfItems();
+        for(auto it:items){
+            delete it;
+        }
+        for (int i = 0; i < shelves.size(); i++)
+        {
+            delete shelves[i];
+        }
+        delete inventory;
+    }
 };
 
-class VendingMachine{
-    private:
-        Inventory *inventory;
-    public:
-        VendingMachine(){}
-        Item* dispenseItem(int itemId){
-            Item* item = inventory->isItemAvailable(itemId);
-            if(item==nullptr){
-                return nullptr;
+class Cart
+{
+private:
+    vector<Item *> itemsInCart;
+
+public:
+    Cart() {}
+    bool addItemInCart(Item *item)
+    {
+        itemsInCart.push_back(item);
+        return true;
+    }
+    bool removeItemFromCart(Item *item)
+    {
+        for (auto it = itemsInCart.begin(); it != itemsInCart.end(); it++)
+        {
+            if (item->getItemId() == (*it)->getItemId())
+            {
+                itemsInCart.erase(it);
+                return true;
             }
-            else{
-                inventory->removeItem(item);
-                return item;
-            }
         }
+        return false;
+    }
+
+    vector<Item *> getItemsInCart()
+    {
+        return itemsInCart;
+    }
 };
 
-class Cart{
-    private:
-        vector<Item*> itemsInCart;
-    public:
-        Cart(){}
-        bool addItemInCart(Item* item){
-            itemsInCart.push_back(item);
-            return true;
-        }
-        bool removeItemFromCart(Item* item){
-            for(auto it=itemsInCart.begin();it!=itemsInCart.end();it++){
-                if(item->getItemId()==(*it)->getItemId()){
-                    itemsInCart.erase(it);
-                    return true;
-                }
-            }
-            return false;
-        }
+class Customer
+{
+private:
+    Cart *cart;
 
-        vector<Item*> getItemsInCart(){
-            return itemsInCart;
-        }
+public:
+    Customer()
+    {
+        cart = new Cart();
+    }
+    bool selectItem(Item *item)
+    {
+        return cart->addItemInCart(item);
+    }
+    bool unselectItem(Item *item)
+    {
+        return cart->removeItemFromCart(item);
+    }
+    Cart *getCart()
+    {
+        return cart;
+    }
+    vector<Item *> getItemsFromCart()
+    {
+        return cart->getItemsInCart();
+    }
+    ~Customer(){
+        delete cart;
+    }
 };
 
-class Customer{
-    private:
-        Cart* cart;
-    public:
-        Customer(){}
-        bool selectItem(Item* item){
-            return cart->addItemInCart(item);
-        }
-        bool unselectItem(Item* item){
-            return cart->removeItemFromCart(item);
-        }
-};
-
-class Checkout{
-    Cart * cart;
+class Checkout
+{
+    Cart *cart;
     int totalAmount;
-    public:
-        Checkout(Cart* cart){
-            this->cart = cart;
-            this->totalAmount = 0;
-        }
-        double calculateTotalAmount(){
-            vector<Item*> itemsInCart = cart->getItemsInCart();
-            if(!itemsInCart.empty()){
-                for(auto it=itemsInCart.begin();it!=itemsInCart.end();it++){
-                    totalAmount+=(*it)->getPrice();
-                }
+
+public:
+    Checkout(Cart *cart)
+    {
+        this->cart = cart;
+        this->totalAmount = 0;
+    }
+    double calculateTotalAmount()
+    {
+        vector<Item *> itemsInCart = cart->getItemsInCart();
+        if (!itemsInCart.empty())
+        {
+            for (auto it = itemsInCart.begin(); it != itemsInCart.end(); it++)
+            {
+                totalAmount += (*it)->getPrice();
             }
-            return totalAmount;
         }
+        return totalAmount;
+    }
+    ~Checkout(){
+        delete cart;
+    }
 };
 
-class Payment{
-    private:
-        bool success;
-    public:
-        Payment(){
-            this->success=false;
-        }
-        bool processPayment(double amount){
-            return true;
-        }
+class Payment
+{
+private:
+    bool success;
+
+public:
+    Payment()
+    {
+        this->success = false;
+    }
+    bool processPayment(double amount)
+    {
+        return true;
+    }
 };
 
-int main(){
-    
-    Inventory* inv = new Inventory();
-    inv->addItem(new Item("RedbullDrink", ItemType::Drink, 120));
-    inv->addItem(new Item("Kurkure", ItemType::Snack, 15));
-    inv->addItem(new Item("50-50", ItemType::Cookie, 25));
+int main()
+{
+    VendingMachine *machine = new VendingMachine();
+    cout << "here" << endl;
+    machine->addShelve(new Shelve(1, ItemType::Drink));
+    machine->addShelve(new Shelve(2, ItemType::Snack));
+    machine->addShelve(new Shelve(3, ItemType::Cookie));
+    cout << "here2" << endl;
+    // Inventory *inv = new Inventory();
+    cout << "here3" << endl;
+
+    Item *item1 = new Item(1, "RedbullDrink", ItemType::Drink, 120);
+    Item *item2 = new Item(2, "Kurkure", ItemType::Snack, 15);
+    Item *item3 = new Item(3, "50-50", ItemType::Cookie, 25);
+    cout << "here4" << endl;
+
+    machine->addItem(item1);
+    machine->addItem(item2);
+    machine->addItem(item3);
+    cout << "here5" << endl;
+
+    Customer *c1 = new Customer();
+    cout << "6" << endl;
+
+    c1->selectItem(item1);
+    cout << "8" << endl;
+
+    c1->selectItem(item3);
+    cout << "here7" << endl;
+
+    Checkout *c = new Checkout(c1->getCart());
+    Payment pay;
+    bool amountPaid = pay.processPayment(c->calculateTotalAmount());
+    if (amountPaid)
+    {
+        vector<Item *> itemsToDispense = c1->getItemsFromCart();
+        for (auto it : itemsToDispense)
+        {
+            machine->dispenseItem((*it).getItemId());
+        }
+    }
+    delete c;
+    delete machine;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
